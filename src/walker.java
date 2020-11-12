1e3169b7742c474547d8816c02dd8d1ba1c945fb
JokeI777 <danleo.2000@mail.ru>
2020-11-12 14:20:25 +0500

    Changed indent.

--- src/walker.java
+++ src/walker.java
@@ -9,20 +9,21 @@ public class walker {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         String path = buffer.readLine();
         File directory = new File(path);
-        walk(directory, 1);
+        if (!directory.isDirectory()){
+            System.out.print("This is not directory!");
+        }
+        else {
+            walk(directory, 1);
+        }
     }
 
     public static void walk(File directory, Integer depth) {
-        if (!directory.isDirectory()) {
-            System.out.print("-".repeat(depth) + directory.getName());
-        } else {
-            for (File file : directory.listFiles()) {
-                if (file.isFile()) {
-                    System.out.println("-".repeat(depth) + file.getName());
-                } else {
-                    System.out.println("-".repeat(depth) + file.getName());
-                    walk(file, depth + 1);
-                }
+        for (File file : directory.listFiles()) {
+            if (file.isFile()) {
+                System.out.println(" ".repeat(depth) + file.getName());
+            } else {
+                System.out.println(" ".repeat(depth) + file.getName());
+                walk(file, depth + 1);
             }
         }
         return;
